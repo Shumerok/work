0b2e80918844ecf18fd1b987ce5ff68538e93659
shumerok <shumerok@gmail.com>
2023-02-10 18:49:00 +0200

    header.blade.php

--- resources/views/includes/main/header.blade.php
+++ resources/views/includes/main/header.blade.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>LARAVEL CRUD</title>
 {{--    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">--}}
     <!-- Google Font: Source Sans Pro -->
